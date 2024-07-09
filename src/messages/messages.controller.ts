cec92213dcc9f2d5606963a7e5fba4850d38468c
Flora Li <samantha20200604@gmail.com>
2024-07-09 19:13:45 +1000

    set up error reporting

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from 'src/messages/dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -25,7 +32,14 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async getMessage(@Param('id') id: string) {
+    // readFile need some amount of time
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
