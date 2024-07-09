0e86547195864fec3958cc6ebb4bdb42ce89d1e0
Flora Li <samantha20200604@gmail.com>
2024-07-09 22:37:50 +1000

    refine MessagesRepo

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -3,10 +3,8 @@ import { MessagesRepository } from './messages.repository';
 export class MessagesService {
   messagesRepo: MessagesRepository;
 
-  constructor() {
-    // Service is creating its own dependencies.
-    // DON'T DO THIS ON REAL APPS
-    this.messagesRepo = new MessagesRepository();
+  constructor(messagesRepo: MessagesRepository) {
+    this.messagesRepo = messagesRepo;
   }
 
   async findOne(id: string) {
